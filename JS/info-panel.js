5fb3e01069ace0d23408bbc18fa36c455c19e32c
InlunaJin <inlunajin@gmail.com>
2022-05-04 11:53:52 +0800

    makeindexusable

---
+++ JS/info-panel.js
@@ -0,0 +1,43 @@
+/* global AFRAME */
+AFRAME.registerComponent('to-link', {
+  init: function () {
+    var buttonEls = document.querySelectorAll('.menu-button');
+    var fadeBackgroundEl = this.fadeBackgroundEl = document.querySelector('#fadeBackground');
+    this.LinkBut = {
+      GoogleButton: {
+       link:'http://www.google.com' 
+      },
+      TwitterButton: {
+        link:'/artthings.html' 
+       },
+       FacebookButton: {
+        link:'https://www.facebook.com' 
+       },
+    };
+
+    this.onMenuButtonClick = this.onMenuButtonClick.bind(this);
+    this.onBackgroundClick = this.onBackgroundClick.bind(this);
+    this.backgroundEl = document.querySelector('#background');
+    for (var i = 0; i < buttonEls.length; ++i) {
+      buttonEls[i].addEventListener('click', this.onMenuButtonClick);
+    }
+    this.backgroundEl.addEventListener('click', this.onBackgroundClick);
+    this.el.object3D.renderOrder = 9999999;
+    this.el.object3D.depthTest = false;
+    fadeBackgroundEl.object3D.renderOrder = 9;
+    fadeBackgroundEl.getObject3D('mesh').material.depthTest = false;
+  },
+
+  onMenuButtonClick: function (evt) {
+
+    var SelectPage = this.LinkBut[evt.currentTarget.id];
+    window.location.href = SelectPage.link;
+  },
+
+  onBackgroundClick: function (evt) {
+    this.backgroundEl.object3D.scale.set(0.001, 0.001, 0.001);
+    this.el.object3D.scale.set(0.001, 0.001, 0.001);
+    this.el.object3D.visible = false;
+    this.fadeBackgroundEl.object3D.visible = false;
+  }
+});
